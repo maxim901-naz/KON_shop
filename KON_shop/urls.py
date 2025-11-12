62df2851480c9af61b956b0d296eb0591cd04c8d
maxim901-naz <nazarov.maxim.200@gmail.com>
2025-11-12 15:57:52 +0400

    Fix static config for Render

--- KON_shop/urls.py
+++ KON_shop/urls.py
@@ -23,8 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls'))
 ]
-
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
