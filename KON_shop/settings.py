5959de9c9e692b70b90323e17239fc48597636be
maxim901-naz <nazarov.maxim.200@gmail.com>
2025-11-13 13:19:57 +0400

    Update CSS and static files

--- KON_shop/settings.py
+++ KON_shop/settings.py
@@ -81,7 +81,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles_new'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # ⚙️ Медиа
 MEDIA_URL = '/media/'
